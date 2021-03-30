126b6043e0a6dc33f5c1f4b1c5d5a8589241c7b3
Prabin Singh <pravin544@gmail.com>
2021-03-29 20:28:15 -0500

    working modal

--- src/components/SelectedBeast.js
+++ src/components/SelectedBeast.js
@@ -6,7 +6,21 @@ class SelectedBeast extends React.Component {
     render() {
         console.log(this.props)
         return (
-            <>Hello</>
+            <>
+            <Modal show={this.props.displayModal} onHide={this.props.hideModal}>
+            <Modal.Dialog>
+              <Modal.Header />
+                <Modal.Body>
+                <Card style={{ width: '25rem'}}>
+                  <Card.Img src={this.props.img} />
+                </Card>
+              </Modal.Body>
+            </Modal.Dialog>
+            <Button variant="primary" onClick={this.props.hideModal}>
+            Close
+          </Button>
+            </Modal>
+            </>
         );
     }
 }
